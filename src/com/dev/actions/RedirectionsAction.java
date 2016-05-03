79dfb500ce320f7b7e89aa79125b312e65e8342e
gauthier <grivenq@gmail.com>
2016-05-03 11:41:17 +0200

    maj2

---
+++ src/com/dev/actions/RedirectionsAction.java
@@ -0,0 +1,23 @@
+package com.dev.actions;
+
+public class RedirectionsAction {
+
+	 public String redirectionProfil() {
+		 
+		    return "profil";
+		  }
+	 
+	 public String redirectionGrille() {
+		 
+		    return "grille";
+		  }
+	 
+	 public String redirectionGroupe() {
+		 
+		    return "groupe";
+		  }
+	 public String redirectionEvaluationCroise() {
+		  
+		    return "evaluation";
+		  }
+}
---
+++ src/dao/LoginAction.java
@@ -0,0 +1,67 @@
+package dao;
+ 
+import org.db.Personnes;
+
+import com.opensymphony.xwork2.ActionSupport;
+ 
+public class LoginAction extends ActionSupport {
+	
+	
+ 
+    private static final long serialVersionUID = 1L;    
+    private String identifiant;
+	private String password;
+	
+	public String getIdentifiant() {
+		return identifiant;
+	}
+
+
+	public void setIdentifiant(String identifiant) {
+		this.identifiant = identifiant;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+    PersonnesDao dao = new PersonnesDao();
+    Personnes personnes;
+ 
+    /*@Override
+    public void validate() {
+        if (personnes.getLogin().length() == (0)) {
+            this.addFieldError("personnes.login", "Name is required");
+        }
+        if (personnes.getPassword().length() == (0)) {
+            this.addFieldError("personnes.password", "Password is required");
+        }
+    }*/
+ 
+    @Override
+    public String execute() {
+        if (dao.find(personnes.getLogin(), personnes.getPassword())) {
+            return "SUCCESS";
+        } else {
+            this.addActionError("Invalid username and password");
+        }
+        return "INPUT";
+    }
+    
+     
+ 
+    public Personnes getPersonnes() {
+        return personnes;
+    }
+ 
+    public void setPersonnes(Personnes personnes) {
+        this.personnes = personnes;
+    }    
+}
